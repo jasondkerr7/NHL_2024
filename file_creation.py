f18589480c18a61fce2028fae65c123013d7dfac
jasondkerr7 <164694209+jasondkerr7@users.noreply.github.com>
2024-11-01 11:32:21 -0400

    Create file_creation.py

---
+++ file_creation.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import os
+import io
+import time
+import re
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload
+from googleapiclient.http import MediaIoBaseUpload
+from googleapiclient.http import MediaFileUpload
+from googleapiclient.errors import HttpError
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+
+# -- GOOGLE CONNECTION -- #
+# Prepare auth json for google connection
+cred_json = os.environ['SERVICE_ACCOUNT_CREDENTIALS_JSON']
+s_char = cred_json.index('~~~')
+e_char = cred_json.index('%%%')
+service_account_cred = eval(cred_json[s_char+3:e_char])
+
+# Connect to the google service account
+scope = ['https://www.googleapis.com/auth/drive']
+credentials = service_account.Credentials.from_service_account_info(
+                              info=service_account_cred, 
+                              scopes=scope)
+ggl_drive = build('drive', 'v3', credentials=credentials)
+
+
+# Test File Creation #
+test_df = pd.DataFrame({'Name':['Jay Decker','et al'],
+                        'Pts':[12,25]})
+
+test_df.to_csv('saved_file.csv')
+
+# Upload File
+returned_fields="id, name, mimeType, webViewLink, exportLinks, parents"
+file_metadata = {'name': 'test_file_creation_nov_1.csv',
+                'parents':['1GTyaZ1tRX1Wrh9LpHGRNoGJo6MWLEqsQ']}
+media = MediaIoBaseUpload('saved_file.csv',
+                        mimetype='text/csv')
+file = ggl_drive.files().create(body=file_metadata, media_body=media,
+                              fields=returned_fields).execute()
